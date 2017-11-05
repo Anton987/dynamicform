f40bcce091f90fe1f5240ad41ddaf031dab72e19
Anton987 <anton.olsson@outlook.com>
2017-11-05 01:10:34 +0100

    Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroFormComponent } from './Components/hero-form/hero-form.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroFormComponent,
+
+
   ],
   imports: [
     BrowserModule
+    ,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
